39700bc65b052bf09230a1568d2e1bada211c118
xmmc <1014198200@qq.com>
2021-06-20 13:26:31 +0800

    koa2  session与redis配置与测试

--- blog-koa2/app.js
+++ blog-koa2/app.js
@@ -5,9 +5,13 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
+const blog = require('./routes/blog')
+const user = require('./routes/user')
 
 // error handler
 onerror(app)
@@ -32,9 +36,29 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
+//session配置
+app.keys = ['wommM12#']
+app.use(session({
+  //配置cookie
+  cookie:{
+    path:'/',
+    httpOnly:true,
+    maxAge:24*60*60*1000
+  },
+  //配置redis
+  store: redisStore({
+    all:'127.0.0.1:6379'    //暂时本地redis redis
+  })
+}))
+
+
+
+
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+app.use(blog.routes(), users.allowedMethods())
+app.use(user.routes(), users.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
